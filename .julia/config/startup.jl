f7ae940de02427d7a50d49b30e710377b0b79f31
Dale-Muccignat <dale.muccignat@my.jcu.edu.au>
2021-11-28 16:50:38 +1100

    added julia

---
+++ .julia/config/startup.jl
@@ -0,0 +1,22 @@
+push!(LOAD_PATH,".")
+push!(LOAD_PATH,"~/OneDrive/PhD/Packages")
+push!(LOAD_PATH,"~/OneDrive/PhD/Scripts")
+using Revise
+ENV["PLOTS_DEFAULT_BACKEND"] = "PyPlot"
+ENV["JULIA_NO_VERIFY_HOSTS"]="github.com"
+ENV["JULIA_DISABLE_AMDGPU_ARTIFACTS"] = 1
+ENV["JULIA_PKG_DEVDIR"]= "/home/mirage_neos/OneDrive/PhD/Packages/"
+ENV["PYTHON"]= "/usr/bin/python3"
+using Plots, JLD2, InteractiveUtils, RemoteREPL, Pluto, DPlots
+InteractiveUtils.define_editor(
+    r"\bemacsclient\b.*\s(-nw|--no-window-system)\b", wait=true) do cmd, path, line
+    `$cmd +$line $path`
+end
+using OhMyREPL
+OhMyREPL.enable_autocomplete_brackets(false)
+OhMyREPL.enable_pass!("RainbowBrackets", true)
+OhMyREPL.enable_highlight_markdown(true)
+OhMyREPL.enable_pass!("RainbowBrackets", true)
+
+
+hpc() = connect_repl("hpc")
