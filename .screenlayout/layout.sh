3598e446ff8bd1ef33af0fc44aa56dd0017130db
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-05-19 08:28:08 +1000

    monitor name change

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DVI-D-1 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 1080x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left
